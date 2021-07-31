b491b70fb1c51ddd17e52e9874b0d8e480dec978
Ayelen Gabrielli <moneet.moneet@gmail.com>
2021-07-31 00:16:29 -0300

    COMMIT UNO

---
+++ app.js
@@ -0,0 +1,18 @@
+let nombre = prompt ("Ingresa Nombre ")
+let apellido = prompt ("Ingresa Apellido")
+let signo = prompt ("De que signo es?")
+
+alert(nombre + " " + apellido + " Usted Está Loco")
+
+console.log (nombre + " " + apellido + " " + signo) 
+
+let dia = prompt("Ingrese El día de Nacimiento")
+
+if (dia <= 10){
+    console.log("Sos Una Persona Tranquila");
+} else if (dia >=25) {
+    console.log("Estas bastante alterad@")
+} else {
+    console.log("Tenes problemas con la Ira")
+}
+
